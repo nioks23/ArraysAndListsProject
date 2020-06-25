7b490b4af22ad682da6a945525754dc7a80130ff
Niokhobaye Diouf <66567989+nioks23@users.noreply.github.com>
2020-06-25 15:48:26 -0400

    Done project

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -9,11 +10,16 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
+            var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
             /* Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
              */
+
+            var evens = new List<int>();
+            var odds = new List<int>();
+
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
@@ -22,11 +28,59 @@ namespace ArraysAndLists
              * or the odds List
              */
 
-            /* Now using foeach or for loops,
+
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    evens.Add(num);
+                }
+                else
+                {
+                    odds.Add(num);
+                }
+
+            }
+
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 == 0)
+            //    {
+            //        evens.Add(numbers[i]);
+            //    }
+            //    else
+            //    {
+            //        odds.Add(numbers[i]);
+            //    }
+            //}
+
+
+            /* Now using foreach or for loops,
              * display each List of even and odd numbers
              *
              * Try to be creative in your display
              */
+            Console.WriteLine("Even Numbers");
+            foreach (var num in numbers)
+            {
+                if(num %2 == 0)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+            Console.WriteLine("---------------------------");
+
+            Console.WriteLine("Odd Numbers");
+
+            foreach (var num in numbers)
+            {
+                if (num % 2 != 0)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+
         }
     }
 }
